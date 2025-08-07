bf150968c891091e5002b0780edeac7fd633d996
sbk283 <sbkcoding@gmail.com>
2025-08-07 11:19:12 +0900

    [docs] useState 의 이해

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,16 @@
-type DemoProps = {
-  name: string;
-  age: number;
-};
+import Counter from "./components/Counter";
+import NameEditor from "./components/NameEditor";
+import ToggleSwitch from "./components/ToggleSwitch";
 
-const Demo = ({ name, age }: DemoProps): JSX.Element => {
+function App(): JSX.Element {
   return (
     <div>
-      <h2>Demo</h2>
-      <div>이름: {name}</div>
-      <div>나이: {age}</div>
+      <h1>useState 예제</h1>
+      <Counter />
+      <NameEditor />
+      <ToggleSwitch />
     </div>
   );
-};
-
-type SampleProps = {
-  children?: React.ReactNode;
-  title: string;
-};
-const Sample = ({ children, title }: SampleProps): JSX.Element => {
-  return (
-    <div>
-      <h2>Sample</h2>
-      <div>{title}</div>
-      <div>{children}</div>
-    </div>
-  );
-};
-
-const App = (): JSX.Element => {
-  return (
-    <div>
-      <h1>App</h1>
-      <Sample title="이것은 Props 중 title 입니다.">
-        <p>나는 Children 입니다.</p>
-      </Sample>
-    </div>
-  );
-};
+}
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-// as 문법은 개발자가 HTMLElement 라고 확신한다는 것을 VSCode 에 알려줌
-// as 문법은 개발자가 null 이 아니라고 VSCode 에 알려줌
+// as 문법은 개발자가 HTMLElement 라고 확신한다는 것을 VSCode 에 알려줌.
+// as 문법은 개발자가 null 이 아니라고 VSCode 에 알려줌.
 const root = ReactDOM.createRoot(
-  // eslint-disable-next-line prettier/prettier
-  document.getElementById("root") as HTMLElement
+  document.getElementById("root") as HTMLElement,
 );
 root.render(<App />);
